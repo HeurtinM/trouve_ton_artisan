949bbc32efa7e8cc12740d86beedf83c2a9e0b12
Heurtin Manu <manu.heurtin@gmail.com>
2026-01-07 12:06:41 +0100

    started the header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ class App extends React.Component {
   render(){
     return(
       <div>
+        <header>
+          <nav className="navbar" style={{ backgroundColor: "#F1F8FC" }}>
+            <img src="/Logo.png" alt="Logo" className="img-fluid" width="300"></img>
+
+
+          </nav>
+        </header>
         <h1>placeholder trouvetonartisan</h1>
       </div>
     )
